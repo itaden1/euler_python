34b9ffbd4f9034b60c8e7e55c9e0d0df99823285
Ethan Shearer <itaden@hotmail.com>
2017-12-13 16:55:31 +1100

    fibonacci

--- tests.py
+++ tests.py
@@ -7,6 +7,9 @@ class Test_euler_answers(unittest.TestCase):
         self.assertEqual(euler.multiples_of_three_and_five(10), 23)
         self.assertEqual(euler.multiples_of_three_and_five(1000), 233168)
 
+    def test_even_fibonacci_numbers(self):
+        self.assertEqual(euler.even_fibonacci_numbers(10),44)
+        self.assertEqual(euler.even_fibonacci_numbers(4000000),44)
 
 if __name__ == '__main__':
     unittest.main()
