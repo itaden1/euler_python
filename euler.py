62fb66ea8004575865a097293d442cecd795e6f4
Ethan Shearer <itaden@hotmail.com>
2017-12-14 10:08:04 +1100

    problem 002 solution

--- euler.py
+++ euler.py
@@ -1,27 +1,17 @@
 from functools import reduce
 
 def multiples_of_three_and_five(n):
-    """
-    Find the sum of all the multiples of 3 or 5 below 1000.
-    """
-    
-    #functional solution
+    """Find the sum of all the multiples of 3 or 5 below 1000."""
     answer = reduce((lambda x, y: x + y), (i for i in range(1,n) if i % 3 == 0 or i % 5 == 0))
     return answer
 
 def fib_gen(max_num):
-    """
-    Generates a fibonacci sequence from 0 to max_num
-    """
-
-    pre,cur = 0,1
+    """Generates a fibonacci sequence from 0 to max_num"""
+    pre,cur = 1,1
     while pre < max_num:
         yield pre
         pre,cur = cur,pre+cur        
 
 def even_fibonacci_numbers(n):
-    """
-    By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
-    """
-
+    """By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms."""
     return reduce((lambda x, y: x + y ),(filter(lambda x: x % 2 == 0,(i for i in fib_gen(4000000)))))
--- tests.py
+++ tests.py
@@ -4,12 +4,19 @@ import euler
 class Test_euler_answers(unittest.TestCase):
 
     def test_multiples_of_three_and_five(self):
+        """test that the sum of multiples of three and five return correct number"""
         self.assertEqual(euler.multiples_of_three_and_five(10), 23)
         self.assertEqual(euler.multiples_of_three_and_five(1000), 233168)
 
+    def test_fibonacci_generator(self):
+        """test to see if a fibonacci sequence is generated"""
+        fib_sequence = [i for i in euler.fib_gen(10)]
+        self.assertEqual(fib_sequence,[1,1,2,3,5,8])
+    
     def test_even_fibonacci_numbers(self):
-        self.assertEqual(euler.even_fibonacci_numbers(10),44)
-        self.assertEqual(euler.even_fibonacci_numbers(4000000),44)
+        """test the sum of all even fibonacci numbers below 4 000 000"""
+        self.assertEqual(euler.even_fibonacci_numbers(10),4613732)
+    
 
 if __name__ == '__main__':
     unittest.main()
