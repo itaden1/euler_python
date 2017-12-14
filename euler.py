5843ac05a4c58e057b4fde45abd5d52053f524c0
Ethan Shearer <itaden@hotmail.com>
2017-12-14 13:20:36 +1100

    three unsolved

--- euler.py
+++ euler.py
@@ -1,9 +1,11 @@
 from functools import reduce
+import math
 
 def multiples_of_three_and_five(n):
     """Find the sum of all the multiples of 3 or 5 below 1000."""
     answer = reduce((lambda x, y: x + y), (i for i in range(1,n) if i % 3 == 0 or i % 5 == 0))
     return answer
+
 
 def fib_gen(max_num):
     """Generates a fibonacci sequence from 0 to max_num"""
@@ -14,4 +16,36 @@ def fib_gen(max_num):
 
 def even_fibonacci_numbers(n):
     """By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms."""
-    return reduce((lambda x, y: x + y ),(filter(lambda x: x % 2 == 0,(i for i in fib_gen(4000000)))))
+    return reduce((lambda x, y: x + y ),(filter(lambda x: x % 2 == 0,(i for i in fib_gen(n)))))
+
+
+def is_prime(n):
+    """check if prime number"""
+    if n > 1:
+        if n == 2:
+            return True
+        for x in range(3, int(math.sqrt(n) + 1), 2):
+            if n % x == 0:
+                return False
+        return True
+    return False
+
+def prime_gen(n):
+    """Generate all primes"""
+    for i in range(2, n):
+        if i == 2:
+            yield i
+        elif i % 2 != 0:
+            yield i
+
+
+def prime_factor_gen(n):
+    """generate a prime factor sequence"""
+    factors = (i for i in range(2,n) if n % i == 0 and is_prime(i))
+    return [i for i in factors]
+
+def largest_prime_factor(n):
+
+    #prime factor generator
+    largest_num = max([i for i in prime_gen(n)])
+    return largest_num
--- tests.py
+++ tests.py
@@ -15,8 +15,25 @@ class Test_euler_answers(unittest.TestCase):
     
     def test_even_fibonacci_numbers(self):
         """test the sum of all even fibonacci numbers below 4 000 000"""
-        self.assertEqual(euler.even_fibonacci_numbers(10),4613732)
-    
+        self.assertEqual(euler.even_fibonacci_numbers(4000000),4613732)
+ 
+    def test_is_prime(self):
+        """test that number is prime"""
+        self.assertTrue(euler.is_prime(7))
+
+    def test_prime_gen(self):
+        prime_list = [i for i in euler.prime_gen(100)]
+        print('primes = {}'.format(prime_list))
+        self.assertEqual(prime_list,[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97])
+
+    def test_prime_factor_gen(self):
+        """test that valid prime factors are generated"""
+        prime_factors = [i for i in euler.prime_factor_gen(13195)]
+        self.assertEqual(prime_factors,[5,7,13,29])
+
+    def test_largest_prime_factor(self):
+        """test the largest prime factor of 600851475143"""
+        #self.assertEqual(euler.largest_prime_factor(600851475143),6857)
 
 if __name__ == '__main__':
     unittest.main()
